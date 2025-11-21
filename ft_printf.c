f1ae3c10934fae24b71a93099717ae7513f3449d
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2025-11-20 23:22:21 -0300

    main function finished

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vguerra- <vguerra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 20:21:16 by vguerra-          #+#    #+#             */
-/*   Updated: 2025/11/20 20:41:55 by vguerra-         ###   ########.fr       */
+/*   Updated: 2025/11/20 20:55:48 by vguerra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int	ft_printf(const char *format, ...)
 {
+	va_list	args;
+	int	i;
 
+	va_start( args, format);
+	while ()
+	{
+		if (args[i])
+		va_args( args, )
+	}
+
+	va_end(args)
 }
