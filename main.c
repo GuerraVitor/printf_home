19798e08b9d3fa9e27907da8e6b24e5648873fbf
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2025-11-20 23:58:44 -0300

    continue with nbrs

--- main.c
+++ main.c
@@ -8,7 +8,15 @@ int main(void)
 
 	len_ft = ft_printf("Meu: %s\n", "Ola 42");
 	len_real = printf("Org: %s\n", "Ola 42");
-	printf("Len Meu: %d | Len Org: %d\n\n", len_ft, len_real);
+	printf("Len Meu s: %d | Len Org: %d\n\n", len_ft, len_real);
+
+	len_ft = ft_printf("Meu: %c\n", "C");
+	len_real = printf("Org: %c\n", "C");
+	printf("Len Meu c: %d | Len Org c: %d\n\n", len_ft, len_real);
+
+	len_ft = ft_printf("Meu: %c\n", "C");
+	len_real = printf("Org: %c\n", "C");
+	printf("Len Meu c: %d | Len Org c: %d\n\n", len_ft, len_real);
 
 	return (0);
 }
