a84880514e4a7fa2a86be9f845fb29a61c7cbd39
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2025-11-20 23:35:22 -0300

    add brs and chars auxiliars prints

---
+++ ft_print_chars.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_chars.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vguerra- <vguerra-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 23:30:32 by vguerra-          #+#    #+#             */
+/*   Updated: 2025/11/20 23:33:42 by vguerra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_print_str(char *str)
+{
+	int	len;
+
+	len = 0;
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[len])
+	{
+		write(1, &str[len], 1);
+		len++;
+	}
+	return (len);
+}
+
--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,54 @@
 /*   By: vguerra- <vguerra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 20:21:16 by vguerra-          #+#    #+#             */
-/*   Updated: 2025/11/20 20:55:48 by vguerra-         ###   ########.fr       */
+/*   Updated: 2025/11/20 23:25:49 by vguerra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
+static int	ft_eval_format(va_list args, const char format)
+{
+	int	len_print;
+
+	len_print = 0;
+	if (format == 'c')
+		len_print += ft_print_char(va_arg(args, int));
+	else if (format == 's')
+		len_print += ft_print_str(va_arg(args, char *));
+	else if (format == 'p')
+		len_print += ft_print_ptr(va_arg(args, unsigned long long));
+	else if (format == 'd' || format == 'i')
+		len_print += ft_print_nbr(va_arg(args, int));
+	else if (format == 'u')
+		len_print += ft_print_unsigned(va_arg(args, unsigned int));
+	else if (format == 'x' || format == 'X')
+		len_print += ft_print_hex(va_arg(args, unsigned int), format);
+	else if (format == '%')
+		len_print += ft_print_char('%');
+	return (len_print);
+
+}
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int	i;
+	int	len_print;
 
+	i = 0;
+	len_print = 0;
 	va_start( args, format);
-	while ()
+	while (format[i])
 	{
-		if (args[i])
-		va_args( args, )
+		if (format[i] == '%')
+		{
+			len_print += ft_eval_format(args, format[i + 1]);
+			i++;
+		}
+		else
+			len_print += ft_print_char(format[i]);
+		i++;
 	}
-
-	va_end(args)
+	va_end(args);
+	return (len_print);
 }
